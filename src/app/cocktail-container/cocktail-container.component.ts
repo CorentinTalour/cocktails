97d78673b9e3bd6d858e1137f65c19a454fd933d
Corentin Talour <corentin.talour@bts-malraux.net>
2024-06-07 17:49:37 +0200

    Add service

--- src/app/cocktail-container/cocktail-container.component.ts
+++ src/app/cocktail-container/cocktail-container.component.ts
@@ -1,44 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {Cocktail} from "../interfaces/cocktail.interface";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Cocktail} from "../shared/interfaces/cocktail.interface";
+import {CocktailService} from "../shared/services/cocktail.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-cocktail-container',
   templateUrl: './cocktail-container.component.html',
   styleUrl: './cocktail-container.component.scss'
 })
-export class CocktailContainerComponent implements OnInit {
-  public cocktails: Cocktail[] = [
-    {
-      name: "Mojito",
-      img:
-        "https://www.hangoverweekends.co.uk/uploads/images/mojito.jpg",
-      description:
-        "The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour."
-    },
-    {
-      name: "Cosmopolitan",
-      img:
-        "https://www.hangoverweekends.co.uk/uploads/images/cosmo.jpg",
-      description:
-        "The tangy concoction of vodka, triple sec, lime juice and cranberry juice has managed to leapfrog the venerable screwdriver as many vodka drinkers prefer the Cosmopolitan’s cleaner and slightly tart taste. The keys to the preparation of a Cosmopolitan are a good brand of cranberry juice and Cointreau Triple Sec, two essential elements to the drink."
-    },
-    {
-      name: "Mai Tai",
-      img:
-        "https://fr.inshaker.com/uploads/cocktail/hires/37/1629720636-%D0%9C%D0%B0%D0%B8%CC%86_%D1%82%D0%B0%D0%B8%CC%86.jpg",
-      description:
-        "The Mai Tai is a Polynesian-style cocktail that has a fruity tropical taste sweet and vibrant. The mixture of light and dark rum, orange curacao, orgeat syrup and lime juice has been a symbol of Tahitian culture ever since the drink was first created."
-    }
-  ];
+export class CocktailContainerComponent implements OnInit, OnDestroy {
+  public cocktails: Cocktail[] = [];
+
+  public subscription: Subscription = new Subscription();
 
   public selectedCocktail: Cocktail;
 
-  public selectCocktail(index: number): void {
-    this.selectedCocktail = this.cocktails[index];
+  constructor(private cocktailService: CocktailService) {
   }
 
+  ngOnInit(): void {
+    this.subscription.add(this.cocktailService.cocktails$.subscribe((cocktails: Cocktail[]) => {
+      this.cocktails = cocktails;
+    }));
 
-  ngOnInit() {
-    this.selectedCocktail = this.cocktails[0];
+    this.subscription.add(this.cocktailService.selectedCocktail$.subscribe((selectedCocktail: Cocktail) => {
+      this.selectedCocktail = selectedCocktail;
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  public selectCocktail(index: number): void {
+    this.cocktailService.selectCocktail(index);
   }
 }
